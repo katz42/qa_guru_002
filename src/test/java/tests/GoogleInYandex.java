ddfe3cca328204d61f40a0fd2b24b2ac42ff7c14
katz42 <4H6TZrUgaBANNWM>
2020-08-02 23:48:58 +0300

    first commit

---
+++ src/test/java/tests/GoogleInYandex.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class GoogleInYandex {
+    @Test
+    void OpenGoogleInYandex () {
+        Configuration.holdBrowserOpen = true;
+
+        // Open Yandex
+        open("https://www.yandex.ru");
+
+        // Search 'Google'
+        $(byName("text")).setValue("Google").pressEnter();
+
+        // Open Google
+        $("#search-result .serp-item a[href='https://www.google.ru/']").click();
+    }
+}
